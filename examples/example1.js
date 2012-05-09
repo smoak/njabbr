e045596008df98a07eea73e6d9aef77de037a188
Scott Moak <scott.moak@gmail.com>
2012-05-09 15:34:20 -0700

    Fixed issue with state after server calls Removed some debugging statements Updated example

--- examples/example1.js
+++ examples/example1.js
@@ -1,8 +1,25 @@
 var jnode = require('../lib/jabbrnode').JabbrNode;
 var jclient = new jnode("http://jabbr-bots.apphb.com/");
+
+jclient.on('messageReceived', function(msg, room) {
+    console.log("[" + msg.When + "] " + msg.User.Name + ": " + msg.Content);
+});
+
 jclient.connect("njabbr", "testing", function(task) {
     console.log("Logged on successfully");
+
     jclient.joinRoom("TheTestRoom", function() {
         console.log("Joined room!");
+
+        jclient.getRoomInfo("TheTestRoom", function(roomInfo) {
+            console.log("Room Info:");
+            console.log("Users in room:");
+            for (var key in roomInfo.Users) {
+                var user = roomInfo.Users[key];
+                console.log(user.Name);
+            }
+        });
+
     });
+
 });

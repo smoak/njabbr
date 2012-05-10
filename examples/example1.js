12de9f117ffedbb82f4ed46fa2b34646d1785ddd
Scott Moak <scott.moak@gmail.com>
2012-05-09 23:38:25 -0700

    Added support for the note command. Removed some debug logging Made some minor refactors

--- examples/example1.js
+++ examples/example1.js
@@ -8,17 +8,19 @@ jclient.on('messageReceived', function(msg, room) {
 jclient.connect("njabbr", "testing", function(task) {
     console.log("Logged on successfully");
 
+    jclient.setNote("Test");
+
     jclient.joinRoom("TheTestRoom", function() {
         console.log("Joined room!");
 
-        jclient.getRoomInfo("TheTestRoom", function(roomInfo) {
+/*        jclient.getRoomInfo("TheTestRoom", function(roomInfo) {
             console.log("Room Info:");
             console.log("Users in room:");
             for (var key in roomInfo.Users) {
                 var user = roomInfo.Users[key];
                 console.log(user.Name);
             }
-        });
+        });*/
 
     });
 

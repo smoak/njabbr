836710bb4385de18862d534f4bae9b5c3d73b3f6
Scott Moak <scott.moak@mybrainoncode.com>
2012-12-06 23:04:03 -0800

    Updated example, added disconnect ability, more features
    Added ability to disconnect properly from the server.
    
    Added ability to set a note, leave a room, and set the gravatar.
    
    Updated example to get rid of comments and use the client in a more
    complete way

--- examples/example1.js
+++ examples/example1.js
@@ -10,38 +10,18 @@ jclient.connect("njabbr", "testing", function(task) {
     console.log("Logged on successfully");
     jclient.joinRoom("Hubot", function() {
       console.log("Joined room!");
+      setTimeout(function() {
+        jclient.say("See ya!", "Hubot");
+
+        setTimeout(function() {
+          jclient.leaveRoom("Hubot", function() {
+            jclient.disconnect();
+          });
+
+        }, 5000);
+      }, 2000);
     });
-    /*console.log("You are currently in the following rooms:");
-    for (var i in task.Result.Rooms) {
-        var room = task.Result.Rooms[i];
-        console.log(room.Name);
-        console.log(room.Private);
-    }
 
-//    jclient.setNote("Test");
-//    jclient.setFlag("US");
-
- //   jclient.joinRoom("Hubot", function() {
- //       console.log("Joined room!");
-   /*     setTimeout(function() {
-            jclient.say("See ya!", "Hubot");
-
-            setTimeout(function() {
-                jclient.leaveRoom("Hubot", function() {
-                    jclient.disconnect();                
-                });
-            }, 5000);
-
-        }, 2000);
-/*        jclient.getRoomInfo("TheTestRoom", function(roomInfo) {
-            console.log("Room Info:");
-            console.log("Users in room:");
-            for (var key in roomInfo.Users) {
-                var user = roomInfo.Users[key];
-                console.log(user.Name);
-            }
-        });*/
-
-  //  });
-
+    jclient.setNote("Test");
+    jclient.setFlag("US");
 });

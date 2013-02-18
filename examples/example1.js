224328f5524cfc98da779c0ac4b7dc77d132cfa4
Sasha Moak <sasha.moak@gmail.com>
2013-02-18 15:10:53 -0800

    Added support for specifying the transport. Also bumped version.

--- examples/example1.js
+++ examples/example1.js
@@ -1,6 +1,6 @@
 var JabbrClient = require('../lib/jabbrclient').JabbrClient;
 var JabbrClientEvents = require('../lib/jabbrclient').JabbrClientEvents;
-var jclient = new JabbrClient("https://jabbr.net");
+var jclient = new JabbrClient("https://jabbr.net", { transport: "longPolling" });
 var util = require('util');
 
 jclient.on(JabbrClientEvents.onMessageReceived, function(msg, room) {
@@ -11,16 +11,6 @@ jclient.connect("njabbr", "testing", function(task) {
     console.log("Logged on successfully");
     jclient.joinRoom("Hubot", function() {
       console.log("Joined room!");
-/*      setTimeout(function() {
-        jclient.say("See ya!", "Hubot");
-
-        setTimeout(function() {
-          jclient.leaveRoom("Hubot", function() {
-            jclient.disconnect();
-          });
-
-        }, 5000);
-      }, 2000);*/
     });
     jclient.on(JabbrClient.onMessageReceived, function(message, room) {
       console.log("Received message: " + message + " in room " + room);
